1cb3e5a17043ca227394a76294d70e6a211faa32
johncog <johnacogan@gmail.com>
2016-08-19 07:46:18 +0100

    adding segmentio tracking, fixing bug

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -27,11 +27,12 @@ class MeasuresController < ApplicationController
     thing_id = UserThingLookup.find(user_thing_lookup_id).thing_id
     thingname = @things.find(thing_id).thingname.to_s
     @measures = @measures.where(active: true).where(thingname: thingname).order(:datetime)
+    user_email = current_user.email
 
     Analytics.track(
-        user_email: current_user.email,
-        event: 'Viewing Measures',
-        properties: { thingname: thingname})
+        user_id: user.id,
+        event: 'Viewed Measures',
+        properties: { thingname: thingname}, {user_email: user_email)
 
     # Remove measures which are inactive
     @measures.all.each do |measure|
